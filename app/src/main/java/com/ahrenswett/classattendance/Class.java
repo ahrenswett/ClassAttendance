eea8eb20f7e53c9ace1c18edeed36c50c2bc9175
ahren <ahrenswett@gmail.com>
2020-01-27 16:10:13 -0800

    Current tragectory of App adding class Class adding main input screen will add roomDB will add student class

---
+++ app/src/main/java/com/ahrenswett/classattendance/Class.java
@@ -0,0 +1,14 @@
+package com.ahrenswett.classattendance;
+
+import java.sql.Time;
+
+public class Class {
+//    Create instance variable
+    String name;
+    int size;
+    String location;
+    Time time
+//    Create Class constructor
+
+
+}
--- app/src/main/java/com/ahrenswett/classattendance/MainActivity.java
+++ app/src/main/java/com/ahrenswett/classattendance/MainActivity.java
@@ -2,6 +2,7 @@ package com.ahrenswett.classattendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -23,44 +25,66 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     static int[] studentArr;
-    boolean allPresent = false;
+    static Boolean allPresent = false;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        View view = MainActivity.this.getWindow().getDecorView();
 
 //        setup scrollview
-        final ScrollView scrollView = findViewById(R.id.scrollView);
+        final ScrollView sv = findViewById(R.id.allStudentsList);
+        final LinearLayout ll = findViewById(R.id.ll);
         final EditText classSize = findViewById(R.id.classSize);
-
 
 //        Set the button to pass the class size to the checkbox generator
         Button submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+//              get size of checkbox list and inittialize an array  in concordance
                 int size = Integer.parseInt(classSize.getText().toString());
                 studentArr = new int[size];
-                final ListView allStudents = new ListView(scrollView.getContext());
-                allStudents.setTop(scrollView.getTop());
-                scrollView.addView(allStudents);
-
 //        create a new checkbox for each student in the class
                 for (int i = 0; i < size; i++) {
-                    CheckBox students = new CheckBox(MainActivity.this);
-                    students.setText("student " + i);
-                    students.setId(i);
-                    allStudents.addView(students);
+                    studentArr[i]=i;
+                }
+                for(final int student:studentArr){
+                    final CheckBox studentBox = new CheckBox(MainActivity.this);
+                    studentBox.setText("Student #"+ student);
+                    studentBox.setId(student);
+                    System.out.println(student);
+                    studentBox.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+//                            check if it was checked if it was check if all checked.
+                            if(studentBox.isChecked()){
+                                allPresent = checker(ll);
+                                Toast.makeText(getApplicationContext(),"status" + allPresent, Toast.LENGTH_SHORT).show();
+                            }else{
+                                allPresent=false;
+                                Toast.makeText(getApplicationContext(),"status" + allPresent, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                    ll.addView(studentBox);
                 }
             }
         });
     }
-
-    private static boolean allCheckedIn(){
-
-        return false;
+//    check if all  checked checked
+    public static boolean checker(LinearLayout ll){
+//        int children = ll.getChildCount();
+        for (int i=0; i<ll.getChildCount(); i++){
+            CheckBox cb = (CheckBox)ll.getChildAt(i);
+            if(!cb.isChecked()){
+               return false;
+            }
+        }
+        return true;
     }
+
 }
